6c94440b3deeaa0c386123d8e217e91992769a4f
VyuginAK <vugasasa85@gmail.com>
2025-04-12 19:15:24 +0300

    Лабораторная работа №1. Задание №4.
    Функция, которая упорядочивает слова в строке по количеству букв в каждом слове

--- LabWork1/Task2-4.py
+++ LabWork1/Task2-4.py
@@ -13,3 +13,11 @@ def is_uppercase_palindrome(s):
     uppercase_chars = [char for char in s if char.isupper()]
     # Проверяем, равен ли список своему обратному варианту
     return uppercase_chars == uppercase_chars[::-1]
+# Задание 14
+def sort_words_by_length(s):
+    # Разбиваем строку на список слов
+    words = s.split()
+    # Сортируем слова по их длине
+    sorted_words = sorted(words, key=lambda x: len(x))
+    # Объединяем отсортированные слова обратно в строку
+    return ' '.join(sorted_words)
