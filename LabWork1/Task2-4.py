9021312e0d03f06255d8735334851e999df7c95d
VyuginAK <vugasasa85@gmail.com>
2025-04-12 19:14:01 +0300

    Лабораторная работа №1. Задание №3
    Функция, которая проверяет, образуют ли прописные символы строки палиндром

--- LabWork1/Task2-4.py
+++ LabWork1/Task2-4.py
@@ -7,3 +7,9 @@ def shuffle_string(s):
     random.shuffle(chars)
     # Объединяем список обратно в строку
     return ''.join(chars)
+# Задание №7
+def is_uppercase_palindrome(s):
+    # Собираем все прописные символы из строки
+    uppercase_chars = [char for char in s if char.isupper()]
+    # Проверяем, равен ли список своему обратному варианту
+    return uppercase_chars == uppercase_chars[::-1]
